9a2390275e517f81b3b393d6d94fe91fc5a346a0
manask322 <manask322@gmail.com>
2020-08-21 00:40:34 +0530

    Added Tests

--- src/middleware/queue_service.go
+++ src/middleware/queue_service.go
@@ -3,15 +3,18 @@ package middleware
 import (
 	"booleanservice/src/models"
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-//ServiceQueueIn is a Buffered Channel to store the incoming requests
-var ServiceQueueIn = make(chan ServiceQueueElement, 100)
+const SERVICEQUEUELENGTH = 10
 
-var ServiceQueueOut = make(chan ServiceQueueElement, 100)
+//ServiceQueueIn is a Buffered Channel to store the incoming requests
+var ServiceQueueIn = make(chan ServiceQueueElement, SERVICEQUEUELENGTH)
+
+var ServiceQueueOut = make(chan ServiceQueueElement, SERVICEQUEUELENGTH)
 
 type ServiceQueueElement struct {
 	C         *gin.Context
@@ -20,16 +23,18 @@ type ServiceQueueElement struct {
 
 //StartQueueJob is
 func StartQueueJob(c *gin.Context, bValue models.NameValue) {
+
 	go processDatabase(bValue)
 }
 
 func processDatabase(bValue models.NameValue) {
 	db := DB
+	fmt.Println("Sasasa")
+	time.Sleep(10 * time.Second)
 	response := db.Create(&bValue)
 	p, _ := json.Marshal(response.Value)
 	var nameValue models.DatabaseNameValue
 	json.Unmarshal(p, &nameValue)
-	time.Sleep(2 * time.Second)
 	queEle := <-ServiceQueueIn
 	queEle.NameValue = nameValue
 	ServiceQueueOut <- queEle
